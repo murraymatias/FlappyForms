621879a8473458226977b6eea36a4905af0a5760
murraymatias <murray.matias@gmail.com>
2019-10-23 06:43:11 -0300

    First commit

---
+++ FlappyForms/FlappyForms/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace FlappyForms
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.moveArea = new System.Windows.Forms.Panel();
+            this.flappyBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.moveArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // moveArea
+            // 
+            this.moveArea.Controls.Add(this.flappyBox);
+            this.moveArea.Location = new System.Drawing.Point(0, 0);
+            this.moveArea.Name = "moveArea";
+            this.moveArea.Size = new System.Drawing.Size(800, 450);
+            this.moveArea.TabIndex = 0;
+            // 
+            // flappyBox
+            // 
+            this.flappyBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.flappyBox.Location = new System.Drawing.Point(250, 150);
+            this.flappyBox.Name = "flappyBox";
+            this.flappyBox.Size = new System.Drawing.Size(30, 30);
+            this.flappyBox.TabIndex = 0;
+            this.flappyBox.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 10;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.moveArea);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.moveArea.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.flappyBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel moveArea;
+        private System.Windows.Forms.PictureBox flappyBox;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ FlappyForms/FlappyForms/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FlappyForms
+{
+    public partial class MainForm : Form
+    {
+        int flappyVelocity = 10;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (flappyBox.Location.Y < moveArea.Height - flappyBox.Height)
+            {
+                flappyBox.Location = new Point(flappyBox.Location.X, flappyBox.Location.Y + flappyVelocity);
+            }
+        }
+    }
+}
