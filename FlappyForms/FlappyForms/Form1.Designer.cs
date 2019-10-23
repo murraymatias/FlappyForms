d8e13369ff7ad76128e3a8d291ec60153cfda61a
murraymatias <murray.matias@gmail.com>
2019-10-23 20:23:24 -0300

    Movimiento, salto y coliciones cuncionando

--- FlappyForms/FlappyForms/Form1.Designer.cs
+++ FlappyForms/FlappyForms/Form1.Designer.cs
@@ -20,6 +20,7 @@
             base.Dispose(disposing);
         }
 
+
         #region Windows Form Designer generated code
 
         /// <summary>
@@ -29,48 +30,213 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.moveArea = new System.Windows.Forms.Panel();
-            this.flappyBox = new System.Windows.Forms.PictureBox();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.moveArea = new System.Windows.Forms.Panel();
+            this.upTube5 = new System.Windows.Forms.PictureBox();
+            this.downTube5 = new System.Windows.Forms.PictureBox();
+            this.upTube2 = new System.Windows.Forms.PictureBox();
+            this.downTube2 = new System.Windows.Forms.PictureBox();
+            this.upTube4 = new System.Windows.Forms.PictureBox();
+            this.downTube4 = new System.Windows.Forms.PictureBox();
+            this.upTube3 = new System.Windows.Forms.PictureBox();
+            this.downTube3 = new System.Windows.Forms.PictureBox();
+            this.upTube = new System.Windows.Forms.PictureBox();
+            this.downTube = new System.Windows.Forms.PictureBox();
+            this.flappyBox = new System.Windows.Forms.PictureBox();
             this.moveArea.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.flappyBox)).BeginInit();
             this.SuspendLayout();
             // 
+            // timer1
+            // 
+            this.timer1.Interval = 15;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
             // moveArea
             // 
+            this.moveArea.Controls.Add(this.upTube5);
+            this.moveArea.Controls.Add(this.downTube5);
+            this.moveArea.Controls.Add(this.upTube2);
+            this.moveArea.Controls.Add(this.downTube2);
+            this.moveArea.Controls.Add(this.upTube4);
+            this.moveArea.Controls.Add(this.downTube4);
+            this.moveArea.Controls.Add(this.upTube3);
+            this.moveArea.Controls.Add(this.downTube3);
+            this.moveArea.Controls.Add(this.upTube);
+            this.moveArea.Controls.Add(this.downTube);
             this.moveArea.Controls.Add(this.flappyBox);
             this.moveArea.Location = new System.Drawing.Point(0, 0);
+            this.moveArea.Margin = new System.Windows.Forms.Padding(4);
             this.moveArea.Name = "moveArea";
-            this.moveArea.Size = new System.Drawing.Size(800, 450);
+            this.moveArea.Size = new System.Drawing.Size(1067, 554);
             this.moveArea.TabIndex = 0;
+            // 
+            // upTube5
+            // 
+            this.upTube5.BackColor = System.Drawing.Color.Transparent;
+            this.upTube5.Image = global::FlappyForms.Properties.Resources.udtube;
+            this.upTube5.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.upTube5.Location = new System.Drawing.Point(910, -166);
+            this.upTube5.Name = "upTube5";
+            this.upTube5.Size = new System.Drawing.Size(72, 422);
+            this.upTube5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upTube5.TabIndex = 10;
+            this.upTube5.TabStop = false;
+            // 
+            // downTube5
+            // 
+            this.downTube5.BackColor = System.Drawing.Color.Transparent;
+            this.downTube5.Image = global::FlappyForms.Properties.Resources.tube;
+            this.downTube5.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.downTube5.Location = new System.Drawing.Point(910, 405);
+            this.downTube5.Name = "downTube5";
+            this.downTube5.Size = new System.Drawing.Size(72, 422);
+            this.downTube5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.downTube5.TabIndex = 9;
+            this.downTube5.TabStop = false;
+            // 
+            // upTube2
+            // 
+            this.upTube2.BackColor = System.Drawing.Color.Transparent;
+            this.upTube2.Image = global::FlappyForms.Properties.Resources.udtube;
+            this.upTube2.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.upTube2.Location = new System.Drawing.Point(600, -219);
+            this.upTube2.Name = "upTube2";
+            this.upTube2.Size = new System.Drawing.Size(72, 422);
+            this.upTube2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upTube2.TabIndex = 8;
+            this.upTube2.TabStop = false;
+            // 
+            // downTube2
+            // 
+            this.downTube2.BackColor = System.Drawing.Color.Transparent;
+            this.downTube2.Image = global::FlappyForms.Properties.Resources.tube;
+            this.downTube2.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.downTube2.Location = new System.Drawing.Point(600, 352);
+            this.downTube2.Name = "downTube2";
+            this.downTube2.Size = new System.Drawing.Size(72, 422);
+            this.downTube2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.downTube2.TabIndex = 7;
+            this.downTube2.TabStop = false;
+            // 
+            // upTube4
+            // 
+            this.upTube4.BackColor = System.Drawing.Color.Transparent;
+            this.upTube4.Image = global::FlappyForms.Properties.Resources.udtube;
+            this.upTube4.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.upTube4.Location = new System.Drawing.Point(400, -219);
+            this.upTube4.Name = "upTube4";
+            this.upTube4.Size = new System.Drawing.Size(72, 422);
+            this.upTube4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upTube4.TabIndex = 6;
+            this.upTube4.TabStop = false;
+            // 
+            // downTube4
+            // 
+            this.downTube4.BackColor = System.Drawing.Color.Transparent;
+            this.downTube4.Image = global::FlappyForms.Properties.Resources.tube;
+            this.downTube4.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.downTube4.Location = new System.Drawing.Point(400, 352);
+            this.downTube4.Name = "downTube4";
+            this.downTube4.Size = new System.Drawing.Size(72, 422);
+            this.downTube4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.downTube4.TabIndex = 5;
+            this.downTube4.TabStop = false;
+            // 
+            // upTube3
+            // 
+            this.upTube3.BackColor = System.Drawing.Color.Transparent;
+            this.upTube3.Image = global::FlappyForms.Properties.Resources.udtube;
+            this.upTube3.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.upTube3.Location = new System.Drawing.Point(200, -219);
+            this.upTube3.Name = "upTube3";
+            this.upTube3.Size = new System.Drawing.Size(72, 422);
+            this.upTube3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upTube3.TabIndex = 4;
+            this.upTube3.TabStop = false;
+            // 
+            // downTube3
+            // 
+            this.downTube3.BackColor = System.Drawing.Color.Transparent;
+            this.downTube3.Image = global::FlappyForms.Properties.Resources.tube;
+            this.downTube3.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.downTube3.Location = new System.Drawing.Point(200, 352);
+            this.downTube3.Name = "downTube3";
+            this.downTube3.Size = new System.Drawing.Size(72, 422);
+            this.downTube3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.downTube3.TabIndex = 3;
+            this.downTube3.TabStop = false;
+            // 
+            // upTube
+            // 
+            this.upTube.BackColor = System.Drawing.Color.Transparent;
+            this.upTube.Image = global::FlappyForms.Properties.Resources.udtube;
+            this.upTube.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.upTube.Location = new System.Drawing.Point(0, -190);
+            this.upTube.Name = "upTube";
+            this.upTube.Size = new System.Drawing.Size(72, 422);
+            this.upTube.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.upTube.TabIndex = 2;
+            this.upTube.TabStop = false;
+            // 
+            // downTube
+            // 
+            this.downTube.BackColor = System.Drawing.Color.Transparent;
+            this.downTube.Image = global::FlappyForms.Properties.Resources.tube;
+            this.downTube.InitialImage = global::FlappyForms.Properties.Resources.tube;
+            this.downTube.Location = new System.Drawing.Point(0, 381);
+            this.downTube.Name = "downTube";
+            this.downTube.Size = new System.Drawing.Size(72, 422);
+            this.downTube.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.downTube.TabIndex = 1;
+            this.downTube.TabStop = false;
             // 
             // flappyBox
             // 
             this.flappyBox.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.flappyBox.Location = new System.Drawing.Point(250, 150);
+            this.flappyBox.Image = global::FlappyForms.Properties.Resources._3429fe6fc982b13dc2fd5dea43d117bb;
+            this.flappyBox.Location = new System.Drawing.Point(314, 195);
+            this.flappyBox.Margin = new System.Windows.Forms.Padding(4);
             this.flappyBox.Name = "flappyBox";
-            this.flappyBox.Size = new System.Drawing.Size(30, 30);
+            this.flappyBox.Size = new System.Drawing.Size(40, 37);
+            this.flappyBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.flappyBox.TabIndex = 0;
             this.flappyBox.TabStop = false;
             // 
-            // timer1
-            // 
-            this.timer1.Enabled = true;
-            this.timer1.Interval = 10;
-            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
             // MainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.moveArea);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "MainForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.MainForm_KeyDown);
             this.moveArea.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.upTube5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.upTube)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.downTube)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.flappyBox)).EndInit();
             this.ResumeLayout(false);
 
@@ -81,6 +247,16 @@
         private System.Windows.Forms.Panel moveArea;
         private System.Windows.Forms.PictureBox flappyBox;
         private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox upTube;
+        private System.Windows.Forms.PictureBox downTube;
+        private System.Windows.Forms.PictureBox upTube2;
+        private System.Windows.Forms.PictureBox downTube2;
+        private System.Windows.Forms.PictureBox upTube4;
+        private System.Windows.Forms.PictureBox downTube4;
+        private System.Windows.Forms.PictureBox upTube3;
+        private System.Windows.Forms.PictureBox downTube3;
+        private System.Windows.Forms.PictureBox upTube5;
+        private System.Windows.Forms.PictureBox downTube5;
     }
 }
 
--- FlappyForms/FlappyForms/Form1.cs
+++ FlappyForms/FlappyForms/Form1.cs
@@ -16,17 +16,130 @@ namespace FlappyForms
         public MainForm()
         {
             InitializeComponent();
-        }
 
-        private void flowLayoutPanel1_Paint(object sender, PaintEventArgs e)
-        {
+            Random r = new Random();
 
+            upTube.Size = new Size(this.Width / 10, this.Height);
+            upTube2.Size = new Size(this.Width / 10, this.Height);
+            upTube3.Size = new Size(this.Width / 10, this.Height);
+            upTube4.Size = new Size(this.Width / 10, this.Height);
+            upTube5.Size = new Size(this.Width / 10, this.Height);
+
+            downTube.Size = new Size(this.Width / 10, this.Height);
+            downTube2.Size = new Size(this.Width / 10, this.Height);
+            downTube3.Size = new Size(this.Width / 10, this.Height);
+            downTube4.Size = new Size(this.Width / 10, this.Height);
+            downTube5.Size = new Size(this.Width / 10, this.Height);
+
+            upTube.Location = new Point(this.Width, r.Next(Convert.ToInt32(-this.Height+this.Height * 0.2), Convert.ToInt32(-this.Height+this.Height * 0.5)));
+            downTube.Location = new Point(this.Width, upTube.Location.Y + Convert.ToInt32(this.Height * 1.3));
+
+            upTube2.Location = new Point(this.Width + this.Width/4, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+            downTube2.Location = new Point(this.Width + this.Width / 4, upTube2.Location.Y + Convert.ToInt32(this.Height * 1.3));
+
+            upTube3.Location = new Point(this.Width + this.Width / 2, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+            downTube3.Location = new Point(this.Width + this.Width / 2, upTube3.Location.Y + Convert.ToInt32(this.Height * 1.3));
+
+            upTube4.Location = new Point(this.Width + this.Width * 3 / 4, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+            downTube4.Location = new Point(this.Width + this.Width * 3 / 4, upTube4.Location.Y + Convert.ToInt32(this.Height * 1.3));
+
+            upTube5.Location = new Point(this.Width * 2, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+            downTube5.Location = new Point(this.Width * 2, upTube5.Location.Y + Convert.ToInt32(this.Height * 1.3));
         }
 
         private void timer1_Tick(object sender, EventArgs e)
         {
+            Random r = new Random();
+
+            //Actualiza posion de flappy
             if (flappyBox.Location.Y < moveArea.Height - flappyBox.Height)
             {
+                flappyBox.Location = new Point(flappyBox.Location.X, flappyBox.Location.Y + flappyVelocity);
+            }
+
+            //Disminuye la velocidad del salto ("gravedad")
+            if(flappyVelocity < 5)
+            {
+                flappyVelocity++;
+            }
+
+            //Avanza los tubos
+            upTube5.Location = new Point(upTube5.Location.X - 2, upTube5.Location.Y);
+            downTube5.Location = new Point(downTube5.Location.X - 2, downTube5.Location.Y);
+
+            upTube4.Location = new Point(upTube4.Location.X - 2, upTube4.Location.Y);
+            downTube4.Location = new Point(downTube4.Location.X - 2, downTube4.Location.Y);
+
+            upTube3.Location = new Point(upTube3.Location.X - 2, upTube3.Location.Y);
+            downTube3.Location = new Point(downTube3.Location.X - 2, downTube3.Location.Y);
+
+            upTube2.Location = new Point(upTube2.Location.X - 2, upTube2.Location.Y);
+            downTube2.Location = new Point(downTube2.Location.X - 2, downTube2.Location.Y);
+
+            upTube.Location = new Point(upTube.Location.X - 2, upTube.Location.Y);
+            downTube.Location = new Point(downTube.Location.X - 2, downTube.Location.Y);
+
+            //Reposicionamiento
+            if(upTube.Location.X <= -this.Width * 0.25)
+            {
+                upTube.Location = new Point(this.Width, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+                downTube.Location = new Point(this.Width, upTube.Location.Y + Convert.ToInt32(this.Height * 1.3));
+            }
+
+            if(upTube2.Location.X <= -this.Width * 0.25)
+            {
+                upTube2.Location = new Point(this.Width, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+                downTube2.Location = new Point(this.Width, upTube2.Location.Y + Convert.ToInt32(this.Height * 1.3));
+            }
+
+            if (upTube3.Location.X <= -this.Width * 0.25)
+            {
+                upTube3.Location = new Point(this.Width, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+                downTube3.Location = new Point(this.Width, upTube3.Location.Y + Convert.ToInt32(this.Height * 1.3));
+            }
+
+            if(upTube4.Location.X <= -this.Width * 0.25)
+            {
+                upTube4.Location = new Point(this.Width, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+                downTube4.Location = new Point(this.Width, upTube4.Location.Y + Convert.ToInt32(this.Height * 1.3));
+            }
+
+            if(upTube5.Location.X <= -this.Width * 0.25)
+            {
+                upTube5.Location = new Point(this.Width, r.Next(Convert.ToInt32(-this.Height + this.Height * 0.2), Convert.ToInt32(-this.Height + this.Height * 0.5)));
+                downTube5.Location = new Point(this.Width, upTube5.Location.Y + Convert.ToInt32(this.Height * 1.3));
+            }
+
+            
+            //Compueba colisiones
+            if((flappyBox.Location.Y <= upTube.Location.Y + upTube.Height && flappyBox.Location.X + flappyBox.Width >= upTube.Location.X && flappyBox.Location.X <= upTube.Location.X + upTube.Width) ||
+                (flappyBox.Location.Y + flappyBox.Height >= downTube.Location.Y && flappyBox.Location.X + flappyBox.Width >= downTube.Location.X && flappyBox.Location.X <= downTube.Location.X + downTube.Width) ||
+                (flappyBox.Location.Y <= upTube2.Location.Y + upTube2.Height && flappyBox.Location.X + flappyBox.Width >= upTube2.Location.X && flappyBox.Location.X <= upTube2.Location.X + upTube2.Width) ||
+                (flappyBox.Location.Y + flappyBox.Height >= downTube2.Location.Y && flappyBox.Location.X + flappyBox.Width >= downTube2.Location.X && flappyBox.Location.X <= downTube2.Location.X + downTube2.Width)||
+                (flappyBox.Location.Y <= upTube3.Location.Y + upTube3.Height && flappyBox.Location.X + flappyBox.Width >= upTube3.Location.X && flappyBox.Location.X <= upTube3.Location.X + upTube3.Width) ||
+                (flappyBox.Location.Y + flappyBox.Height >= downTube3.Location.Y && flappyBox.Location.X + flappyBox.Width >= downTube3.Location.X && flappyBox.Location.X <= downTube3.Location.X + downTube3.Width) ||
+                (flappyBox.Location.Y <= upTube4.Location.Y + upTube4.Height && flappyBox.Location.X + flappyBox.Width >= upTube4.Location.X && flappyBox.Location.X <= upTube4.Location.X + upTube4.Width) ||
+                (flappyBox.Location.Y + flappyBox.Height >= downTube4.Location.Y && flappyBox.Location.X + flappyBox.Width >= downTube4.Location.X && flappyBox.Location.X <= downTube4.Location.X + downTube4.Width) ||
+                (flappyBox.Location.Y <= upTube5.Location.Y + upTube5.Height && flappyBox.Location.X + flappyBox.Width >= upTube5.Location.X && flappyBox.Location.X <= upTube5.Location.X + upTube5.Width) ||
+                (flappyBox.Location.Y + flappyBox.Height >= downTube5.Location.Y && flappyBox.Location.X + flappyBox.Width >= downTube5.Location.X && flappyBox.Location.X <= downTube5.Location.X + downTube5.Width))
+            {
+                timer1.Stop();
+                MessageBox.Show("Sos un boludo", "Perdiste", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); 
+            }
+        }
+
+        private void MainForm_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Arranca el timer con la tecla S
+            if(e.KeyCode == Keys.S)
+            {
+                timer1.Start();
+            }
+
+            //Genera el salto de flappy modificando su velocidad de movimiento
+            if(e.KeyCode == Keys.Space)
+            {
+                flappyVelocity = -6;
                 flappyBox.Location = new Point(flappyBox.Location.X, flappyBox.Location.Y + flappyVelocity);
             }
         }
